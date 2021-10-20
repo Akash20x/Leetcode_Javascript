883bf1bb67b959f8527cebe7066c7c81430e7a44
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-10-21 02:19:39 +0530

    Update Best_Time_to_Buy_and_Sell_Stock.js

--- Best Time to Buy and Sell Stock/Best_Time_to_Buy_and_Sell_Stock.js
+++ Best Time to Buy and Sell Stock/Best_Time_to_Buy_and_Sell_Stock.js
@@ -5,8 +5,8 @@ condition that we cannot sell the stock on the day on which
 we buy the stock and we can sell it on another day in future 
 after the buying day. To get the maximum profit first we have 
 to choose the day which has lowest price of stock so that we can
-sutract from maximum value to get maximum profit. Let we have
-given prices of stock of 7 days. For choosing minimum price stock 
+sutract from maximum price of the stock to get maximum profit. Let
+we have given prices of stock of 7 days. For choosing minimum price stock 
 we can simply choose minimum value from the list of prices but 
 what if minimum value is on the last 7th day then we didn't have
 any day to sell the stock so our minimum value cannot be fixed 
